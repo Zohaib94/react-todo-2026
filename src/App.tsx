aafef080eab72d228bd3530aafea46f335622b98
Zohaib <zohaibahmedbutt@gmail.com>
2026-02-13 15:32:12 +0200

    add edit form

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import { Route, Routes } from 'react-router';
 import TodosPage from './pages/Todos';
 import TodoPage from './pages/Todo';
 import NewTodoPage from './pages/NewTodo';
+import EditTodoPage from './pages/EditTodo';
 
 function App() {
   return (
     <Routes>
       <Route path="/todos/new" element={<NewTodoPage />} />
+      <Route path="/todos/:id/edit" element={<EditTodoPage />} />
       <Route path="/todos/:id" element={<TodoPage />} />
       <Route path="/" element={<TodosPage />} />
     </Routes>
--- src/pages/EditTodo/index.tsx
+++ src/pages/EditTodo/index.tsx
@@ -1,7 +1,68 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import type { Todo } from '../../types';
+import TodoForm from '../../components/TodoForm';
+import type { TodoFormInput } from '../../schemas/todo';
 
 function EditTodoPage() {
-  return <div>Edit todo page</div>;
+  const [todo, setTodo] = useState<Todo>();
+  const [isLoading, setIsLoading] = useState<boolean>();
+  const [errorMessage, setErrorMessage] = useState<string>();
+  const { id } = useParams();
+
+  const fetchTodo = useCallback(async (): Promise<Todo> => {
+    const response = await axios.get(`https://dummyjson.com/todos/${id}`);
+    return response.data;
+  }, [id]);
+
+  const updateTodo = useCallback(
+    async (data: TodoFormInput): Promise<Todo> => {
+      const response = await axios.put(`https://dummyjson.com/todos/${id}`, data, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      return response.data;
+    },
+    [id],
+  );
+
+  useEffect(() => {
+    const getTodo = async (): Promise<void> => {
+      try {
+        const data = await fetchTodo();
+        setTodo(data);
+        setIsLoading(false);
+      } catch (err: unknown) {
+        const errorMessage = err instanceof Error ? err.message : JSON.stringify(err);
+        console.log(errorMessage);
+        setIsLoading(false);
+        setErrorMessage(errorMessage);
+      }
+    };
+
+    getTodo();
+  }, [fetchTodo]);
+
+  return (
+    <>
+      {isLoading && <span>Loading....</span>}
+      {errorMessage && <span>{errorMessage}</span>}
+      {todo && (
+        <TodoForm
+          todo={{
+            todo: todo.todo,
+            completed: todo.completed,
+            userId: todo.userId,
+          }}
+          formAction="update"
+          formSubmissionRequest={updateTodo}
+        />
+      )}
+    </>
+  );
 }
 
 export default EditTodoPage;
